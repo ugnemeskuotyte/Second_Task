e8612fb1a78a8003bda44633b384003c30a830d2
ugnemeskuotyte <45960501+ugnemeskuotyte@users.noreply.github.com>
2022-05-08 19:37:09 +0300

    Delete v1.1 directory

--- v1.1/skaic.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "Studentas.h"
-
-double Vidurkis(vector<int> paz);
-double Mediana(vector<int> paz);
-void skaiciavimas(vector<Studentas>& sarasas, int arVM);
-
-void skaiciavimas(list<Studentas>& sarasas, int arVM);
-void skaiciavimas(deque<Studentas>& sarasas, int arVM);
