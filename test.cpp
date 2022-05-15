00a4805118db6a808ee59c306b49aed6d7b78076
ugnemeskuotyte <45960501+ugnemeskuotyte@users.noreply.github.com>
2022-05-16 00:43:16 +0300

    testai

---
+++ test.cpp
@@ -0,0 +1,49 @@
+#include "pch.h"
+#include "..//v1.5/Studentas.h"
+#include "..//v1.5/generavimas.h"
+#include "..//v1.5/skaic.h"
+#include "..//v1.5/studentas.cpp"
+#include "..//v1.5/skaic.cpp"
+#include "..//v1.5/generavimas.cpp"
+#include "..//v1.5/zmogus.h"
+#include "..//v1.5/zmogus.cpp"
+
+TEST(TestCaseName, TestName) {
+  EXPECT_EQ(1, 1);
+  EXPECT_TRUE(true);
+}
+TEST(studentasTest, VardasTest)
+{
+	Studentas studentas;
+	string vardas = "Vardenis";
+	studentas.setName(vardas);
+	EXPECT_EQ(studentas.getVardas(), "Vardenis");
+}
+TEST(studentasTest, egzaminasTest)
+{
+	string vardas = "Vardenis";
+	string pavarde = "Pavarde";
+	Studentas s;
+	s.setName(vardas);
+	s.setSurname(pavarde);
+	int x = 5;
+	s.setEgz(x);
+	EXPECT_EQ(s.getEgz(), 5);
+}
+TEST(studentasTest, vidurkisTest)
+{
+	string vardas = "Vardenis";
+	string pavarde = "Pavarde";
+	Studentas s;
+	s.setName(vardas);
+	s.setSurname(pavarde);
+	int paz;
+	for (int i = 0; i < 10; i++)
+	{
+		s.setMark(10);
+	}
+	s.setEgz(10);
+	s.setResult(Vidurkis(s.getMarks()));
+	double ats = s.getResult() * 0.4 + s.getEgz() * 0.6;
+	EXPECT_EQ(ats, 10);
+}
